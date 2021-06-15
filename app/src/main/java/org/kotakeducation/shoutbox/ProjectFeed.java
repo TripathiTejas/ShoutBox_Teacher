6655903252be50954172f8ff2b7ed827f088c335
TripathiTejas <tejastripathi07@gmail.com>
2021-06-15 19:28:29 +0530

    Initial commit

---
+++ app/src/main/java/org/kotakeducation/shoutbox/ProjectFeed.java
@@ -0,0 +1,14 @@
+package org.kotakeducation.shoutbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_feed);
+    }
+}
