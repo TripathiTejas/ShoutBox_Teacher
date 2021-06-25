a830c1e193987049905a1cb600046fb460df8469
TripathiTejas <tejastripathi07@gmail.com>
2021-06-25 19:43:46 +0530

    All work till update screen done.

---
+++ app/src/main/java/org/kotakeducation/shoutbox/ModelForProjectFeed.java
@@ -0,0 +1,21 @@
+package org.kotakeducation.shoutbox;
+
+public class ModelForProjectFeed {
+
+    String Title,Username,Date,UserId,ProjectID;
+
+    public ModelForProjectFeed(String Title,String Username,String Date,String UserID,String ProjectID){
+        this.Title=Title;
+        this.Username=Username;
+        this.Date=Date;
+        this.UserId=UserID;
+        this.ProjectID=ProjectID;
+    }
+
+    public String getTitle(){return Title;}
+    public String getUsername(){return Username;}
+    public String getDate(){return Date;}
+    public  String getUserId(){return UserId;}
+    public  String getProjectID(){return ProjectID;}
+
+}
