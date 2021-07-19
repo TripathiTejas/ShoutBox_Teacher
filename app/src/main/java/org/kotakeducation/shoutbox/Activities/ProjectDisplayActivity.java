2673e8c7cb34ea8a7c077b4bb1ab34ed049e4090
purab-bak <purab.bak@gmail.com>
2021-07-20 01:48:59 +0530

    View Project UI

--- app/src/main/java/org/kotakeducation/shoutbox/Activities/ProjectDisplayActivity.java
+++ app/src/main/java/org/kotakeducation/shoutbox/Activities/ProjectDisplayActivity.java
@@ -2,6 +2,7 @@ package org.kotakeducation.shoutbox.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -32,14 +35,18 @@ import com.google.firebase.storage.StorageReference;
 import org.jetbrains.annotations.NotNull;
 import org.kotakeducation.shoutbox.R;
 
+import java.util.HashMap;
+
 public class ProjectDisplayActivity extends AppCompatActivity {
 
-    private TextView projectTitle,projectDesc;
+    private TextView projectTitle, projectDesc;
     private ImageView projectImage;
-    private Button deleteProject,updateProject, comments;
-    private String UserID,ProjectID;
+
+    private FloatingActionButton deleteProject, comments;
+    ExtendedFloatingActionButton updateProject;
+    private String UserID, ProjectID;
     private FirebaseFirestore db;
-    private StorageReference reference= FirebaseStorage.getInstance().getReference();
+    private StorageReference reference = FirebaseStorage.getInstance().getReference();
 
     TextView enquiryDetailsTV;
 
@@ -57,24 +64,24 @@ public class ProjectDisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_display);
 
-        projectTitle=findViewById(R.id.ProjectTitle);
-        projectDesc=findViewById(R.id.ProjectDescription);
-        projectImage=findViewById(R.id.ProjectImage);
-        deleteProject=findViewById(R.id.DeleteProject);
-        updateProject=findViewById(R.id.UpdateProject);
-        comments=findViewById(R.id.Comments);
-        db=FirebaseFirestore.getInstance();
+        projectTitle = findViewById(R.id.ProjectTitle);
+        projectDesc = findViewById(R.id.ProjectDescription);
+        projectImage = findViewById(R.id.ProjectImage);
+        deleteProject = findViewById(R.id.DeleteProject);
+        updateProject = findViewById(R.id.UpdateProject);
+        comments = findViewById(R.id.Comments);
+        db = FirebaseFirestore.getInstance();
 
-        enquiryDetailsTV=findViewById(R.id.enquiryDetailsTV);
+        enquiryDetailsTV = findViewById(R.id.enquiryDetailsTV);
 
         likeButton = findViewById(R.id.likeImage);
         likeCountTV = findViewById(R.id.likeCount);
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
 
-        Intent intent=getIntent();
-        UserID=intent.getStringExtra("User ID");
-        ProjectID=intent.getStringExtra("Project Id");
+        Intent intent = getIntent();
+        UserID = intent.getStringExtra("User ID");
+        ProjectID = intent.getStringExtra("Project Id");
 
         likesRef = FirebaseDatabase.getInstance().getReference().child("project-likes").child(ProjectID);
 
@@ -84,6 +91,9 @@ public class ProjectDisplayActivity extends AppCompatActivity {
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
                 count = snapshot.getChildrenCount();
                 likeCountTV.setText(String.valueOf(count));
+                if (snapshot.child(mCurrentUser.getUid()).exists()){
+                    likeButton.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.dark_blue));
+                }
             }
 
             @Override
@@ -98,13 +108,12 @@ public class ProjectDisplayActivity extends AppCompatActivity {
                 likesRef.child(mCurrentUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                        if (!snapshot.exists()){
+                        if (!snapshot.exists()) {
                             likesRef.child(mCurrentUser.getUid()).setValue(mCurrentUser.getUid());
-                            Toast.makeText(ProjectDisplayActivity.this, "Liked!", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
+                            likeButton.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.dark_blue));
+                        } else {
                             likesRef.child(mCurrentUser.getUid()).removeValue();
-                            Toast.makeText(ProjectDisplayActivity.this, "Like Removed!", Toast.LENGTH_SHORT).show();
+                            likeButton.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.grey));
                         }
                     }
 
@@ -116,14 +125,14 @@ public class ProjectDisplayActivity extends AppCompatActivity {
             }
         });
 
-        display(UserID,ProjectID);
+        display(UserID, ProjectID);
 
         comments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(ProjectDisplayActivity.this, Comments.class);
-                intent.putExtra("User ID",UserID);
-                intent.putExtra("Project Id",ProjectID);
+                Intent intent = new Intent(ProjectDisplayActivity.this, Comments.class);
+                intent.putExtra("User ID", UserID);
+                intent.putExtra("Project Id", ProjectID);
                 startActivity(intent);
             }
         });
@@ -131,9 +140,9 @@ public class ProjectDisplayActivity extends AppCompatActivity {
         updateProject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(ProjectDisplayActivity.this, EditProjectActivity.class);
-                intent.putExtra("User ID",UserID);
-                intent.putExtra("Project Id",ProjectID);
+                Intent intent = new Intent(ProjectDisplayActivity.this, EditProjectActivity.class);
+                intent.putExtra("User ID", UserID);
+                intent.putExtra("Project Id", ProjectID);
                 startActivity(intent);
                 finish();
             }
@@ -146,39 +155,39 @@ public class ProjectDisplayActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void aVoid) {
                         db.collection("Projects").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        for(DocumentSnapshot snapshot : task.getResult()){
-                                            if(snapshot.getString("User ID").equals(UserID) && snapshot.getString("Project Id").equals(ProjectID)){
-                                                StorageReference fileref=reference.child(snapshot.getString("Image ID"));
-                                                fileref.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                for (DocumentSnapshot snapshot : task.getResult()) {
+                                    if (snapshot.getString("User ID").equals(UserID) && snapshot.getString("Project Id").equals(ProjectID)) {
+                                        StorageReference fileref = reference.child(snapshot.getString("Image ID"));
+                                        fileref.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                db.collection("Projects").document(snapshot.getId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                                                     @Override
                                                     public void onSuccess(Void aVoid) {
-                                                        db.collection("Projects").document(snapshot.getId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                            @Override
-                                                            public void onSuccess(Void aVoid) {
-                                                                Toast.makeText(ProjectDisplayActivity.this, "Project Deleted Successfully", Toast.LENGTH_SHORT).show();
-                                                                Intent intent = new Intent(ProjectDisplayActivity.this, ProjectFeedActivity.class);
-                                                                startActivity(intent);
-                                                                finish();
-                                                            }
-                                                        }).addOnFailureListener(new OnFailureListener() {
-                                                            @Override
-                                                            public void onFailure(@NonNull Exception e) {
-                                                                Toast.makeText(ProjectDisplayActivity.this, "Check your Internet Connection", Toast.LENGTH_SHORT).show();
-                                                            }
-                                                        });
+                                                        Toast.makeText(ProjectDisplayActivity.this, "Project Deleted Successfully", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(ProjectDisplayActivity.this, ProjectFeedActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
                                                     }
                                                 }).addOnFailureListener(new OnFailureListener() {
                                                     @Override
-                                                    public void onFailure(@NonNull Exception exception) {
+                                                    public void onFailure(@NonNull Exception e) {
                                                         Toast.makeText(ProjectDisplayActivity.this, "Check your Internet Connection", Toast.LENGTH_SHORT).show();
                                                     }
                                                 });
                                             }
-                                        }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception exception) {
+                                                Toast.makeText(ProjectDisplayActivity.this, "Check your Internet Connection", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
                                     }
-                                }).addOnFailureListener(new OnFailureListener() {
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(ProjectDisplayActivity.this, "Check your Internet Connection", Toast.LENGTH_SHORT).show();
@@ -196,19 +205,26 @@ public class ProjectDisplayActivity extends AppCompatActivity {
 
     }
 
-    public void display(String UserID,String ProjectID){
+    public void display(String UserID, String ProjectID) {
 
         db.collection(UserID).get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        for(DocumentSnapshot snapshot : task.getResult()){
-                            if(ProjectID.equals(snapshot.getId())) {
+                        for (DocumentSnapshot snapshot : task.getResult()) {
+                            if (ProjectID.equals(snapshot.getId())) {
                                 projectTitle.setText(snapshot.getString("Project Title"));
-                                projectDesc.setText(snapshot.getString("Project Desc"));
+                                projectDesc.append(snapshot.getString("Project Desc"));
 
-                                enquiryDetailsTV.setText(snapshot.get("enquiryDetails").toString());
-
+                                //predict, plan, investigate, record, analyze, connect;
+                                HashMap<String, String> map = (HashMap) snapshot.get("enquiryDetails");
+                                enquiryDetailsTV.setText("Question : "+ map.get("question"));
+                                enquiryDetailsTV.append("\n\nPrediction : " + map.get("predict"));
+                                enquiryDetailsTV.append("\n\nPlanning : " + map.get("plan"));
+                                enquiryDetailsTV.append("\n\nInvestigation : " + map.get("investigate"));
+                                enquiryDetailsTV.append("\n\nRecording : " + map.get("record"));
+                                enquiryDetailsTV.append("\n\nAnalysis : " + map.get("analyze"));
+                                enquiryDetailsTV.append("\n\nConnection : " + map.get("connect"));
                                 Glide.with(getApplicationContext()).load(snapshot.getString("Project Image")).centerCrop().into(projectImage);
                                 break;
                             }
