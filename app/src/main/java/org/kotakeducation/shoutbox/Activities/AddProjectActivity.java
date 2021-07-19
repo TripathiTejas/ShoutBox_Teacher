44312799c6975266c3fe27bc41533b239403c7d5
purab-bak <purab.bak@gmail.com>
2021-07-20 00:55:06 +0530

    Add Project UI

--- app/src/main/java/org/kotakeducation/shoutbox/Activities/AddProjectActivity.java
+++ app/src/main/java/org/kotakeducation/shoutbox/Activities/AddProjectActivity.java
@@ -20,7 +20,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -48,11 +51,13 @@ import java.util.Map;
 
 public class AddProjectActivity extends AppCompatActivity {
 
-    private EditText ProjectTitle,ProjectDesc;
+    private EditText ProjectTitle;
+    TextInputEditText ProjectDesc;
     private ImageView projectImage;
     private Uri imageUri;
-    private StorageReference reference= FirebaseStorage.getInstance().getReference();
-    private Button UploadProject, addEnquiry;
+    private StorageReference reference = FirebaseStorage.getInstance().getReference();
+    private MaterialButton addEnquiry;
+    ExtendedFloatingActionButton UploadProject;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     EnquiryProjectModel model;
@@ -64,18 +69,18 @@ public class AddProjectActivity extends AppCompatActivity {
 
         addEnquiry = findViewById(R.id.addEnquiry);
 
-        if (getIntent().getStringExtra("activity").equals("enquiry")){
-            model= (EnquiryProjectModel) getIntent().getExtras().getSerializable("enquiryModel");
+        if (getIntent().getStringExtra("activity").equals("enquiry")) {
+            model = (EnquiryProjectModel) getIntent().getExtras().getSerializable("enquiryModel");
             Toast.makeText(this, model.getQuestion(), Toast.LENGTH_SHORT).show();
             addEnquiry.setText("Enquiry Added!");
             addEnquiry.setEnabled(false);
         }
 
-        ProjectTitle=findViewById(R.id.ProjectTitle);
-        ProjectDesc=findViewById(R.id.ProjectDescription);
-        projectImage=findViewById(R.id.ProjectImage);
-        UploadProject=findViewById(R.id.AddProject);
-        db=FirebaseFirestore.getInstance();
+        ProjectTitle = findViewById(R.id.ProjectTitle);
+        ProjectDesc = findViewById(R.id.ProjectDescription);
+        projectImage = findViewById(R.id.ProjectImage);
+        UploadProject = findViewById(R.id.AddProject);
+        db = FirebaseFirestore.getInstance();
 
         projectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,27 +88,29 @@ public class AddProjectActivity extends AppCompatActivity {
                 Intent gallerayIntent = new Intent();
                 gallerayIntent.setAction(Intent.ACTION_GET_CONTENT);
                 gallerayIntent.setType("image/*");
-                startActivityForResult(gallerayIntent,2);
+                startActivityForResult(gallerayIntent, 2);
             }
         });
 
         UploadProject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String projectTitle=ProjectTitle.getText().toString();
-                String projectDesc=ProjectDesc.getText().toString();
+                String projectTitle = ProjectTitle.getText().toString();
+                String projectDesc = ProjectDesc.getText().toString();
+
                 // check for empty fields
-                if(imageUri== null)
-                    Toast.makeText(AddProjectActivity.this, "Please add an Image", Toast.LENGTH_SHORT).show();
-                else if(projectTitle.isEmpty())
-                    Toast.makeText(AddProjectActivity.this, "Please add a Title", Toast.LENGTH_SHORT).show();
-                else if(projectDesc.isEmpty())
-                    Toast.makeText(AddProjectActivity.this, "Please add a Description", Toast.LENGTH_SHORT).show();
-                else if (model == null){
+                if (model == null) {
                     Toast.makeText(AddProjectActivity.this, "Add Enquiry Details First!", Toast.LENGTH_SHORT).show();
-                }
+                } else if (imageUri == null)
+                    Toast.makeText(AddProjectActivity.this, "Please add an Image", Toast.LENGTH_SHORT).show();
+                else if (projectTitle.isEmpty())
+                    Toast.makeText(AddProjectActivity.this, "Please add a Title", Toast.LENGTH_SHORT).show();
+                else if (projectDesc.isEmpty())
+                    Toast.makeText(AddProjectActivity.this, "Please add a Description", Toast.LENGTH_SHORT).show();
                 else {
-                    uploadToStorage(imageUri,projectTitle,projectDesc);
+                    Toast.makeText(AddProjectActivity.this, "Uploading! Please wait", Toast.LENGTH_SHORT).show();
+
+                    uploadToStorage(imageUri, projectTitle, projectDesc);
                 }
             }
         });
@@ -113,32 +120,32 @@ public class AddProjectActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==2 && resultCode==RESULT_OK && data!=null){
-            imageUri=data.getData();
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
             projectImage.setImageURI(imageUri);
         }
     }
 
-    private void uploadToStorage(Uri uri,String projectTitle,String projectDesc){
+    private void uploadToStorage(Uri uri, String projectTitle, String projectDesc) {
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        String userID= currentUser.getUid();
+        String userID = currentUser.getUid();
 
-        Long tsLong = System.currentTimeMillis()/1000;
+        Long tsLong = System.currentTimeMillis() / 1000;
         String ts = tsLong.toString();
 
-        String ImageId = userID+" "+ts;
+        String ImageId = userID + " " + ts;
 
-        StorageReference fileref=reference.child(ImageId);
+        StorageReference fileref = reference.child(ImageId);
         fileref.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                 fileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
-                        String imageURL=uri.toString();
-                        saveToUserList(projectTitle,projectDesc,userID,imageURL,ImageId);
+                        String imageURL = uri.toString();
+                        saveToUserList(projectTitle, projectDesc, userID, imageURL, ImageId);
                     }
                 });
             }
@@ -150,11 +157,11 @@ public class AddProjectActivity extends AppCompatActivity {
         });
     }
 
-    private void saveToUserList(String projectTitle,String projectDesc,String userID,String imageURL,String ImageId){
+    private void saveToUserList(String projectTitle, String projectDesc, String userID, String imageURL, String ImageId) {
         Map<String, Object> map = new HashMap<>();
-        map.put("Project Title",projectTitle);
-        map.put("Project Desc",projectDesc);
-        map.put("Project Image",imageURL);
+        map.put("Project Title", projectTitle);
+        map.put("Project Desc", projectDesc);
+        map.put("Project Image", imageURL);
         map.put("enquiryDetails", model);
 
 
@@ -165,7 +172,7 @@ public class AddProjectActivity extends AppCompatActivity {
                     public void onSuccess(DocumentReference documentReference) {
                         String projectID = documentReference.getId();
                         String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-                        saveToCommonList(userID,projectID,projectTitle,date,ImageId, model);
+                        saveToCommonList(userID, projectID, projectTitle, date, ImageId, model);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -176,21 +183,21 @@ public class AddProjectActivity extends AppCompatActivity {
                 });
     }
 
-    private void saveToCommonList(String userID, String projectID, String projectTitle, String date, String ImageID, EnquiryProjectModel model){
+    private void saveToCommonList(String userID, String projectID, String projectTitle, String date, String ImageID, EnquiryProjectModel model) {
 
         db.collection("Teacher Info").get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        for(DocumentSnapshot snapshot : task.getResult()){
-                            if(userID.equals(snapshot.getId())) {
+                        for (DocumentSnapshot snapshot : task.getResult()) {
+                            if (userID.equals(snapshot.getId())) {
                                 Map<String, Object> map = new HashMap<>();
-                                map.put("User ID",userID);
-                                map.put("Project Id",projectID);
-                                map.put("Project Title",projectTitle);
-                                map.put("Date of Upload",date);
-                                map.put("Image ID",ImageID);
-                                map.put("User Name",snapshot.getString("FULL NAME"));
+                                map.put("User ID", userID);
+                                map.put("Project Id", projectID);
+                                map.put("Project Title", projectTitle);
+                                map.put("Date of Upload", date);
+                                map.put("Image ID", ImageID);
+                                map.put("User Name", snapshot.getString("FULL NAME"));
                                 map.put("enquiryDetails", model);
 
                                 db.collection("Projects")
